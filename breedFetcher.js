7950aa210078742581bc237f87349974d7a8d5f8
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-04 22:31:34 +0000

    Callback works, added more comments, everything runs smoothly!

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,19 +6,21 @@ const breedFetcher = (breedName) => {
   //Website is hardcoded as it is a constant, only thing that changes is the breed name
   const websiteUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
   request(websiteUrl, (error, response, body) => {
-    //If an error occurs, run an error message
+    //If an error occurs, run an error message and cancels program, else it continues with search
     if (error) {
-      (`Error, failure to search. \n${error}\nPlease try again.`)
-    }
-    //Converts string into an object
-    const data = JSON.parse(body);
-    //checks if the body was able to be parsed, and successfully turned into an object within an
-    if (data[0]) {
-      // Prints out only the description of the breed"
-      console.log(data[0].description);
+      console.log(`Error, failure to search.\nPlease try again.\nReport:\n${error}`)
+      return
     } else {
-      //If the breed name isnt found, log a message saying that it could not be found.
-      console.log(`Sorry, could not find ${breedName}. Please try again.`)
+      //Converts string into an object
+      const data = JSON.parse(body);
+      //checks if the body was able to be parsed, and successfully turned into an object within an
+      if (data[0]) {
+        // Prints out only the description of the breed"
+        console.log(data[0].description);
+      } else {
+        //If the breed name isnt found, log a message saying that it could not be found.
+        console.log(`Sorry, could not find ${breedName}. Please try again.`)
+      }
     }
   })
 };

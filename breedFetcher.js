fecb3a7dca06047feeb0046e3f3efec43ae887e7
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-04 23:14:17 +0000

    linted code

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,25 +4,25 @@ const request = require('./node_modules/request');
 //const breedName = arg[2]
 const fetchBreedDescription = function(breedName, callback) {
   //Website is hardcoded as it is a constant, only thing that changes is the breed name
-  const websiteUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+  const websiteUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(websiteUrl, (error, response, body) => {
     //If an error occurs, run an error message and cancels program, else it continues with search
     if (error) {
-      callback (error, desc)
+      callback(error);
     } else {
       //Converts string into an object
       const data = JSON.parse(body);
       //checks if the body was able to be parsed, and successfully turned into an object within an
       if (data[0]) {
         // Prints out only the description of the breed"
-         let desc = data[0].description
-         callback (error, desc)
+        let desc = data[0].description;
+        callback(error, desc);
       //} else {
       //  //If the breed name isnt found, log a message saying that it could not be found.
       //  console.log(`Sorry, could not find ${breedName}. Please try again.`)
       }
     }
-  })
+  });
 };
 
 module.exports = { fetchBreedDescription };

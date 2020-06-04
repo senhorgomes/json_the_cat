c9aa0498e8530b2a06c7d3613805bfe9b852ebbf
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-04 22:19:00 +0000

    Final version of breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,24 @@
 const request = require('./node_modules/request');
 const arg = process.argv
+//allows you to search it through the terminal. Automatically sets the breed name as breedName
 const breedName = arg[2]
 const breedFetcher = (breedName, callback) => {
+  //Website is hardcoded as it is a constant, only thing that changes is the breed name
   const websiteUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
   request(websiteUrl, (error, response, body) => {
+    //If an error occurs, run an error message
     if (error) {
-      callback (`Error, failure to search. ${error}. Please try again.`)
+      callback (`Error, failure to search. \n${error}\nPlease try again.`)
     }
+    //Converts string into an object
     const data = JSON.parse(body);
-    if (breedName) {
+    //checks if the body was able to be parsed, and successfully turned into an object within an array.
+    if (data[0]) {
+      // Prints out only the description of the breed"
       console.log(data[0].description);
+    } else {
+      //If the breed name isnt found, log a message saying that it could not be found.
+      console.log(`Sorry, could not find ${breedName}. Please try again.`)
     }
   })
 };
